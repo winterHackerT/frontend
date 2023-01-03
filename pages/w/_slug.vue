4925ab7ef059ae43e66c5e715dc510c94e08fd24
YeJun, Jung <yejun614@naver.com>
2023-01-04 01:27:37 +0900

    Feat: add tooltip for star

--- pages/w/_slug.vue
+++ pages/w/_slug.vue
@@ -6,7 +6,13 @@
 
         <nav>
           <ul>
-            <li><NuxtLink to="#" class="btn"><i class="bi bi-star"></i> 0</NuxtLink></li>
+            <li>
+              <NuxtLink to="#" class="btn star">
+                <i class="bi bi-star"></i> 0
+                <span class="tooltip">Star</span>
+              </NuxtLink>
+            </li>
+
             <li><NuxtLink to="#" class="btn">역링크</NuxtLink></li>
             <li><NuxtLink to="#" class="btn">토론</NuxtLink></li>
             <li><NuxtLink to="#" class="btn">편집</NuxtLink></li>
@@ -69,11 +75,57 @@ export default defineComponent({
             list-style: none;
             float:left;
 
+            position: relative;
+
             a {
               font-size: 14px;
               color: black;
               text-decoration: blink;
               padding: 8px 15px;
+            }
+
+            .star.btn {
+              .tooltip {
+                color: white;
+                font-size: 18px;
+
+                padding: 5px 20px;
+                display: none;
+
+                position: absolute;
+                bottom: 100%;
+                left: 0;
+                margin-bottom: 20px;
+
+                background-color: black;
+                border-radius: 20px;
+              }
+
+              .tooltip::after {
+                content: '';
+
+                width: 0;
+                height: 0;
+                border-left: 10px solid transparent;
+                border-right: 10px solid transparent;
+                border-top: 10px solid black;
+
+                position: absolute;
+                bottom: -10px;
+                left: 50%;
+                margin-left: -10px;
+              }
+
+              .bi-star {
+                color: red;
+                margin-right: 5px;
+              }
+            }
+
+            .star.btn:hover {
+              .tooltip {
+                display: inline;
+              }
             }
           }
         }
