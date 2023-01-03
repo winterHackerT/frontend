6d0da0059ce62f65eb257a9e05fa17b1e34f05ae
YeJun, Jung <yejun614@naver.com>
2023-01-04 00:03:30 +0900

    Feat: add list box

---
+++ components/ListBox.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="list-box">
+    <h2>{{ title }}</h2>
+
+    <ul>
+      <li v-for="item, index in items" :key="index">
+        <NuxtLink :to="item.link">{{ item.title }}</NuxtLink>
+        <span v-if="item.meta" class="meta">{{ item.meta }}</span>
+      </li>
+    </ul>
+
+    <NuxtLink class="more-btn" :to="moreLink">[ 더 보기 ]</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: 'Title'
+    },
+    items: {
+      type: Array as PropType<any>,
+      default: () => ([
+        {
+          title: 'title',
+          link: '#',
+          meta: 'meta',
+        }
+      ]),
+    },
+    moreLink: {
+      type: String,
+      default: '#'
+    }
+  },
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/css/variable.scss';
+
+.list-box {
+  border: 1px solid $light-light;
+  background-color: white;
+
+  a {
+    color: $light-primary;
+    text-decoration: blink;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  h2 {
+    font-size: 22px;
+
+    padding: 18px 15px;
+    border-bottom: 1px solid $light-light;
+  }
+
+  ul {
+    li {
+      padding: 5px 10px;
+      list-style: none;
+      border-bottom: 1px solid $light-light;
+
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+
+      a {
+        font-size: 16px;
+      }
+    }
+  }
+
+  .more-btn {
+    float: right;
+    padding: 10px 15px;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
+  <div id="layout">
     <GlobalNavigationBar />
-    <Nuxt />
+
+    <main>
+      <content>
+        <Nuxt />
+      </content>
+
+      <aside>
+        <ListBox title="최근 변경" :items="recentChanges" />
+        <ListBox title="최근 게시물" :items="recentPosts" />
+      </aside>
+    </main>
   </div>
 </template>
 
@@ -11,10 +21,112 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   setup () {
     return {}
+  },
+  data () {
+    return {
+      recentChanges: [
+        {
+          title: 'test test test test test test1',
+          link: '#',
+          meta: '23:50',
+        },
+        {
+          title: 'test test test test test test1',
+          link: '#',
+          meta: '23:50',
+        },
+        {
+          title: 'test test test test test test1',
+          link: '#',
+          meta: '23:50',
+        },
+        {
+          title: 'test test test test test test1',
+          link: '#',
+          meta: '23:50',
+        },
+      ],
+      recentPosts: [
+        {
+          title: 'test test test test test test test test2',
+          link: '#',
+        },
+        {
+          title: 'test test test test test test test test2',
+          link: '#',
+        },
+        {
+          title: 'test test test test test test test test2',
+          link: '#',
+        },
+        {
+          title: 'test test test test test test test test2',
+          link: '#',
+        },
+      ],
+    }
   }
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '@/assets/css/variable.scss';
 
+#layout {
+  min-height: 100vh;
+  background-color: $background;
+
+  main {
+    width: 100%;
+    max-width: 1200px;
+    min-height: 100%;
+
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: row;
+
+    > * {
+      border: 1px solid $light-light
+    }
+
+    content {
+      width: 100%;
+      min-height: 100vh;
+      
+      background-color: white;
+    }
+
+    aside {
+      width: 400px;
+
+      margin-left: 15px;
+
+      display: flex;
+      flex-direction: column;
+      
+      .list-box {
+        margin-top: 20px;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: $max-table-width) {
+  #layout {
+    main {
+      flex-direction: column;
+
+      aside {
+        width: 100%;
+        margin: 0;
+
+        .list-box {
+          margin: 0;
+          border: none;
+        }
+      }
+    }
+  }
+}
 </style>
