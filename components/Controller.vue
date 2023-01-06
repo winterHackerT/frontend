a267a690bc41edd24f79851293396930a5fa0022
tomorrow9913 <tomorrow9913@gmail.com>
2023-01-06 15:31:25 +0900

    Refactor : Controller 컴포넌트로 분리
    - 컨트롤러를 컴포넌트로 분리하였음.

---
+++ components/Controller.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="controller">
+    <button class="btn"><i class="bi bi-chevron-left"></i>Prev</button>
+    <button class="btn">Next<i class="bi bi-chevron-right"></i></button>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    prev: {
+      type: String,
+      default: ''
+    },
+    next: {
+      type: String,
+      default: ''
+    }
+  },
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.controller {
+  .btn {
+    float: left;
+    margin: 0;
+    padding: 7px;
+  }
+}
+</style>
