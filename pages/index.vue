7afd61950aca34528eeaa14fbf6217e2fba07cdc
YeJun, Jung <yejun614@naver.com>
2023-01-07 16:38:08 +0900

    Feat: add redirect in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,5 @@
 <template>
-  <div id="document-view-page">
-    <document-title
-      :document-title="documentTitle"
-      recent-edit="20XX-XX-XX XX:XX:XX"
-    >
-      <li>
-        <NuxtLink to="#" class="btn star">
-          <i class="bi bi-star"></i> 0
-          <span class="tooltip">Star</span>
-        </NuxtLink>
-      </li>
-
-      <li>
-        <NuxtLink :to="`/backlink/${documentTitle}`" class="btn">역링크</NuxtLink>
-      </li>
-      <li><NuxtLink :to="`/discuss/${documentTitle}`" class="btn">토론</NuxtLink></li>
-      <li><NuxtLink to="#" class="btn">편집</NuxtLink></li>
-      <li><NuxtLink :to="`/history/${documentTitle}`" class="btn">역사</NuxtLink></li>
-      <li><NuxtLink to="#" class="btn">ACL</NuxtLink></li>
-    </document-title>
-
-    <classification-block />
+  <div>
 
   </div>
 </template>
@@ -29,14 +8,15 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  setup() {
+  setup () {
     return {}
   },
-  data() {
-    return {
-      documentTitle: 'HackWiki:대문',
-    }
-},})
+  created() {
+    this.$router.push(`/w/${this.$accessor.title}:대문`);
+  }
+})
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+
+</style>
