3af273a90c83344af7bdbfa7abf4c3465bb6a759
tomorrow9913 <tomorrow9913@gmail.com>
2023-01-07 09:29:27 +0900

    Feat : 이전 버전 문서일 경우 이전 버전 알림 박스 추가
    Feat : 새로운 기능 추가
    Fix : 버그 수정
    Docs : 문서의 수정
    Style : (코드의 수정 없이) 스타일(style)만 변경(들여쓰기 같은 포맷이나 세미콜론을 빼먹은 경우)
    Refactor : 코드를 리펙토링
    Test : Test 관련한 코드의 추가, 수정
    Chore : (코드의 수정 없이) 설정을 변경
    
    더 자세한 묘사가 필요한 경우 작성합니다. 약 72자 이내의 길이로 작성합니다. 
    첫 행은 커밋의 대상으로 나머지 문장은 그에 대한 서술로 채웁니다. 본문과 제목을 띄우는 것은 중요합니다. 
    
    해당 커밋이 해결하고 있는 문제 설명. 
    코드의 변화로 알 수 있는 어떻게 변경 사항을 만들었는지 보다는 왜 변경했는지에 대한 서술이 더 중요합니다.
    만약 부작용이나 본문 만으로 직관적으로 서술이 되지 않는다면 다음과 같은 공간을 활용한다.
    
    더 많은 추가 단락은 빈 줄을 넣고 활용한다.
    
     - 글머리 기호 활용도 가능하다.
    
     - 일반적으로 하이픈(-)이나 에스터리스크(*)를 활용한다.
    
    만약 이슈 추적기를 사용한다면 마지막에 아래와 같이 활용한다.
    
    Resolves: #num
    See also: #num, #num

--- components/DocumentTitle.vue
+++ components/DocumentTitle.vue
@@ -8,168 +8,164 @@
             <small v-if="pageName != ''"> ({{ pageName }})</small>
           </h1>
         </NuxtLink>
-
         <nav>
           <ul>
             <slot></slot>
           </ul>
         </nav>
       </div>
-
-      <span v-if="recentEdit != ''" id="last-modified-datetime">
-          최근 수정 시간: {{ recentEdit }}
-      </span>
+      <span v-if="editTime != '' && !documentVersion" id="last-modified-datetime">
+              최근 수정 시간: {{ editTime }}
+          </span>
+      <div class="noticeBox" v-if="documentVersion" :id="documentVersion?'prevVer':''">
+        <b>[주의!]</b> 문서의 이전 버전({{ editTime }}에 수정)을 보고 있습니다.<NuxtLink :to="`/w/${documentTitle}`">최신 버전으로 이동</NuxtLink>
+      </div>
     </section>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  props: {
-    documentTitle: {
-      type: String,
-      default: 'document title',
+  import {
+    defineComponent
+  } from 'vue'
+  export default defineComponent({
+    props: {
+      documentTitle: {
+        type: String,
+        default: 'document title',
+      },
+      pageName: {
+        type: String,
+        default: '',
+      },
+      editTime: {
+        type: String,
+        default: '',
+      },
     },
-    pageName: {
-      type: String,
-      default: '',
+    setup() {
+      return {}
     },
-    recentEdit: {
-      type: String,
-      default: '',
-    },
-  },
-  setup() {
-    return {}
-  },
-})
+    data(){
+      return{
+        documentVersion: this.$route.query?.rev
+      }
+    }
+  })
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/css/variable.scss';
-
-#document-title {
-  margin-bottom: 30px;
-
-  #title {
-    .top {
-      margin-bottom: 10px;
-
-      display: flex;
-      flex-direction: row;
-      align-items: flex-start;
-      justify-content: space-between;
-
-      h1 {
-        font-size: 35px;
-        overflow-wrap: anywhere;
-        text-decoration: none;
-
-        small {
-          font-weight: normal;
+  @import '@/assets/css/variable.scss';
+  #document-title {
+    margin-bottom: 30px;
+    #title {
+      .top {
+        margin-bottom: 10px;
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        justify-content: space-between;
+        h1 {
+          font-size: 35px;
+          overflow-wrap: anywhere;
+          text-decoration: none;
+          small {
+            font-weight: normal;
+          }
         }
-      }
-
-      nav {
-        position: relative;
-        top: 6px;
-
-        flex-grow: 0;
-        flex-shrink: 0;
-
-        ul {
-          li {
-            list-style: none;
-            float: left;
-
-            position: relative;
-
-            a {
-              font-size: 14px;
-              color: black;
-              text-decoration: blink;
-              padding: 8px 15px;
-            }
-
-            .star.btn {
-              .tooltip {
-                color: white;
-                font-size: 18px;
-
-                padding: 5px 20px;
-                display: none;
-
-                position: absolute;
-                bottom: 100%;
-                left: 0;
-                margin-bottom: 20px;
-
-                background-color: black;
-                border-radius: 20px;
+        nav {
+          position: relative;
+          top: 6px;
+          flex-grow: 0;
+          flex-shrink: 0;
+          ul {
+            li {
+              list-style: none;
+              float: left;
+              position: relative;
+              a {
+                font-size: 14px;
+                color: black;
+                text-decoration: blink;
+                padding: 8px 15px;
               }
-
-              .tooltip::after {
-                content: '';
-
-                width: 0;
-                height: 0;
-                border-left: 10px solid transparent;
-                border-right: 10px solid transparent;
-                border-top: 10px solid black;
-
-                position: absolute;
-                bottom: -10px;
-                left: 50%;
-                margin-left: -10px;
+              .star.btn {
+                .tooltip {
+                  color: white;
+                  font-size: 18px;
+                  padding: 5px 20px;
+                  display: none;
+                  position: absolute;
+                  bottom: 100%;
+                  left: 0;
+                  margin-bottom: 20px;
+                  background-color: black;
+                  border-radius: 20px;
+                }
+                .tooltip::after {
+                  content: '';
+                  width: 0;
+                  height: 0;
+                  border-left: 10px solid transparent;
+                  border-right: 10px solid transparent;
+                  border-top: 10px solid black;
+                  position: absolute;
+                  bottom: -10px;
+                  left: 50%;
+                  margin-left: -10px;
+                }
+                .bi-star {
+                  color: red;
+                  margin-right: 5px;
+                }
               }
-
-              .bi-star {
-                color: red;
-                margin-right: 5px;
-              }
-            }
-
-            .star.btn:hover {
-              .tooltip {
-                display: inline;
+              .star.btn:hover {
+                .tooltip {
+                  display: inline;
+                }
               }
             }
           }
         }
       }
-    }
-
-    #last-modified-datetime {
-      font-size: 14px;
-      float: right;
+      #last-modified-datetime {
+        font-size: 14px;
+        float: right;
+      }
     }
   }
-}
-
-@media screen and (max-width: $max-table-width) {
-  #document-title {
-    #title {
-      padding-top: 30px;
-
-      .top {
-        nav {
-          width: 100%;
-          padding: 7px 0;
-
-          position: absolute;
-          top: 0;
-          right: 0;
-
-          border-bottom: 1px solid $light-light;
-          background-color: white;
-
-          display: flex;
-          justify-content: flex-end;
+  @media screen and (max-width: $max-table-width) {
+    #document-title {
+      #title {
+        padding-top: 30px;
+        .top {
+          nav {
+            width: 100%;
+            padding: 7px 0;
+            position: absolute;
+            top: 0;
+            right: 0;
+            border-bottom: 1px solid $light-light;
+            background-color: white;
+            display: flex;
+            justify-content: flex-end;
+          }
         }
       }
     }
   }
-}
+  .noticeBox {
+    border-radius: 0.25rem;
+    font-size: .9rem;
+    margin-bottom: 1.4rem;
+    padding: 0.5rem 0.8rem;
+    
+    border: 1px solid #bcdff1;
+    color: #31708f;
+  }
+  #prevVer {
+    background-color: #f2dede;
+    border-color: #ebcccc;
+    color: #a94442;
+  }
 </style>
