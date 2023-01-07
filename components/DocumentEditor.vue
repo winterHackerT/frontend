49378e763b8f47569a1a7a20aebf9d6cb7ea4797
YeJun, Jung <yejun614@naver.com>
2023-01-07 18:00:52 +0900

    Feat: tab bar

--- components/DocumentEditor.vue
+++ components/DocumentEditor.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="document-editor">
     <div class="tab-bar">
-      <div class="left">
-        <button class="btn tab">편집기</button>
-        <button class="btn tab">RAW 편집</button>
-        <button class="btn tab">미리보기</button>
+      <div ref="tabs" class="left">
+        <button class="btn tab active" @click="setTab(0)">편집기</button>
+        <button class="btn tab" @click="setTab(1)">RAW 편집</button>
+        <button class="btn tab" @click="setTab(2)">미리보기</button>
       </div>
 
       <div class="right">
@@ -21,10 +21,10 @@
       </div>
     </div>
 
-    <main>
-      <!-- <section v-ref=""></section>
-      <section v-ref=""></section>
-      <section v-ref=""></section> -->
+    <main ref="main">
+      <section class="active">TAB 1</section>
+      <section>TAB 2</section>
+      <section>TAB 3</section>
     </main>
   </div>
 </template>
@@ -35,10 +35,79 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   setup () {
     return {}
+  },
+  methods: {
+    setTab(index: number) {
+      const tabs = this.$refs.tabs as HTMLElement;
+      const mainElement = this.$refs.main as HTMLElement;
+
+      tabs.querySelector(".active")?.classList.remove("active");
+      tabs.querySelectorAll(".btn")[index].classList.add("active");
+
+      mainElement.querySelector(".active")?.classList.remove("active");
+      mainElement.querySelectorAll("section")[index].classList.add("active");
+    }
   }
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '@/assets/css/variable.scss';
 
+.document-editor {
+  .tab-bar {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    .btn {
+      padding: 10px 15px;
+    }
+
+    .left {
+      .tab {
+        border: 1px solid transparent;
+        border-bottom: none;
+        border-radius: 4px 4px 0 0;
+      }
+
+      .tab:hover {
+        background-color: transparent;
+      }
+      
+      .tab.active {
+        border: 1px solid $light-light;
+        border-bottom: none;
+      }
+    }
+
+    .right {
+      .editor-toolbar {
+        .btn {
+          border: none;
+          font-weight: bold;
+        }
+      }
+    }
+  }
+
+  main {
+    width: 100%;
+    min-height: 500px;
+    border: 1px solid $light-light;
+
+    section {
+      display: none;
+    }
+
+    section.active {
+      display: inline;
+    }
+  }
+}
+
+@media screen and (max-width: $max-table-width) {
+  
+}
 </style>
