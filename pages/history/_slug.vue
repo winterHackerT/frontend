79132dacb9b30d1648cbeb5a7428cd4f6d5fd385
YeJun, Jung <yejun614@naver.com>
2023-01-07 16:02:43 +0900

    Style: change indents

--- pages/history/_slug.vue
+++ pages/history/_slug.vue
@@ -21,120 +21,135 @@
     </div>
   </template>
   
-  <script lang="ts">
-  import { defineComponent } from 'vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface DocumentHistroy {
+  addr: String,
+  datetime: String,
+  id: Number,
+  length: Number,
+  message: String,
+  order: Number,
+  username: String,
+  working: String,
+  diff: number
+};
   
-  export default defineComponent({
-    setup() {
-      return {}
-    },
-    data() {
-      return {
-        documentTitle: this.$route.params.slug as any,
-        documentItems: [{
+export default defineComponent({
+  setup() {
+    return {}
+  },
+  data() {
+    return {
+      documentTitle: this.$route.params.slug as any,
+      documentItems: [
+        {
           editDateTime: '2021-08-01 00:00:00', 
           editVersion: 1,
           editor: 'admin'
-        },{
+        },
+        {
           editDateTime: '2021-08-01 00:00:00', 
           editVersion: 2,
           editor: 'test'
-        },{
+        },
+        {
           editDateTime: '2021-08-01 00:00:00', 
           editVersion: 3,
           editor: 'test1'
-        }]
-      }
-    },
-  })
-  </script>
-  
-  <style lang="scss" scoped>
-  #document-view-page {
-    #title {
-      .top {
-        margin-bottom: 10px;
-  
-        display: flex;
-        flex-direction: row;
-        align-items: flex-start;
-        justify-content: space-between;
-  
-        h1 {
-          font-size: 35px;
         }
-  
-        nav {
-          position: relative;
-          top: 6px;
-  
-          ul {
-            li {
-              list-style: none;
-              float: left;
-  
-              position: relative;
-  
-              a {
-                font-size: 14px;
-                color: black;
-                text-decoration: blink;
-                padding: 8px 15px;
+      ]
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+#document-view-page {
+  #title {
+    .top {
+      margin-bottom: 10px;
+
+      display: flex;
+      flex-direction: row;
+      align-items: flex-start;
+      justify-content: space-between;
+
+      h1 {
+        font-size: 35px;
+      }
+
+      nav {
+        position: relative;
+        top: 6px;
+
+        ul {
+          li {
+            list-style: none;
+            float: left;
+
+            position: relative;
+
+            a {
+              font-size: 14px;
+              color: black;
+              text-decoration: blink;
+              padding: 8px 15px;
+            }
+
+            .star.btn {
+              .tooltip {
+                color: white;
+                font-size: 18px;
+
+                padding: 5px 20px;
+                display: none;
+
+                position: absolute;
+                bottom: 100%;
+                left: 0;
+                margin-bottom: 20px;
+
+                background-color: black;
+                border-radius: 20px;
               }
-  
-              .star.btn {
-                .tooltip {
-                  color: white;
-                  font-size: 18px;
-  
-                  padding: 5px 20px;
-                  display: none;
-  
-                  position: absolute;
-                  bottom: 100%;
-                  left: 0;
-                  margin-bottom: 20px;
-  
-                  background-color: black;
-                  border-radius: 20px;
-                }
-  
-                .tooltip::after {
-                  content: '';
-  
-                  width: 0;
-                  height: 0;
-                  border-left: 10px solid transparent;
-                  border-right: 10px solid transparent;
-                  border-top: 10px solid black;
-  
-                  position: absolute;
-                  bottom: -10px;
-                  left: 50%;
-                  margin-left: -10px;
-                }
-  
-                .bi-star {
-                  color: red;
-                  margin-right: 5px;
-                }
+
+              .tooltip::after {
+                content: '';
+
+                width: 0;
+                height: 0;
+                border-left: 10px solid transparent;
+                border-right: 10px solid transparent;
+                border-top: 10px solid black;
+
+                position: absolute;
+                bottom: -10px;
+                left: 50%;
+                margin-left: -10px;
               }
-  
-              .star.btn:hover {
-                .tooltip {
-                  display: inline;
-                }
+
+              .bi-star {
+                color: red;
+                margin-right: 5px;
+              }
+            }
+
+            .star.btn:hover {
+              .tooltip {
+                display: inline;
               }
             }
           }
         }
       }
-  
-      #last-modified-datetime {
-        font-size: 14px;
-        float: right;
-      }
+    }
+
+    #last-modified-datetime {
+      font-size: 14px;
+      float: right;
     }
   }
-  </style>
-  
+}
+</style>
