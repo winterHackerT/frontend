09815a9bca0877dfd2bf48bbe816ef746c26429f
YeJun, Jung <yejun614@naver.com>
2023-01-08 04:16:48 +0900

    Fix: top value in nav

--- components/DocumentTitle.vue
+++ components/DocumentTitle.vue
@@ -147,7 +147,7 @@
           width: 100%;
           padding: 7px 0;
           position: absolute;
-          top: 0;
+          top: -2px;
           right: 0;
           border-bottom: 1px solid $light-light;
           background-color: white;
