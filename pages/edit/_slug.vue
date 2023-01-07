4cd86ea5e14af9ac6eb4f3c3659af8104d388aa7
YeJun, Jung <yejun614@naver.com>
2023-01-08 03:58:22 +0900

    Feat: add fetch error in edit page

--- pages/edit/_slug.vue
+++ pages/edit/_slug.vue
@@ -9,30 +9,36 @@
       <li><NuxtLink to="#" class="btn">이동</NuxtLink></li>
     </document-title>
 
-    <div>
-      <DocumentEditor ref="editor" v-model="content" />
+    <div v-if="!isFetchError">
+      <div class="editor">
+        <DocumentEditor ref="editor" v-model="content" />
+      </div>
+
+      <div class="message">
+        <label>요약</label>
+        <input v-model="message" type="text" />
+      </div>
+
+      <div class="extra">
+        <p>
+          <input ref="checkTerms" type="checkbox" />
+          <span>
+            문서 편집을 <b>저장</b>하면 당신은 기여한 내용을 <b>CC-BY-NC-SA 2.0 KR</b>으로 배포하고 기여한 문서에 대한 하이퍼링크나 URL을 이용해서 저작자 표기를 하는 것으로 충분하다는 데 동의하는 것입니다. 이 동의는 <b>철회할 수 없습니다.</b>
+          </span>
+        </p>
+
+        <p>
+          <span class="ip-warn">비로그인 상태로 편집합니다. 편집 역사에 IP(0.0.0.0)가 영구히 기록됩니다.</span>
+        </p>
+      </div>
+
+      <div class="btn-group">
+        <button class="btn submit" @click="saveDocument()">저장</button>
+      </div>
     </div>
 
-    <div class="message">
-      <label>요약</label>
-      <input v-model="message" type="text" />
-    </div>
-
-    <div class="extra">
-      <p>
-        <input ref="checkTerms" type="checkbox" />
-        <span>
-          문서 편집을 <b>저장</b>하면 당신은 기여한 내용을 <b>CC-BY-NC-SA 2.0 KR</b>으로 배포하고 기여한 문서에 대한 하이퍼링크나 URL을 이용해서 저작자 표기를 하는 것으로 충분하다는 데 동의하는 것입니다. 이 동의는 <b>철회할 수 없습니다.</b>
-        </span>
-      </p>
-
-      <p>
-        <span class="ip-warn">비로그인 상태로 편집합니다. 편집 역사에 IP(0.0.0.0)가 영구히 기록됩니다.</span>
-      </p>
-    </div>
-
-    <div class="btn-group">
-      <button class="btn submit" @click="saveDocument()">저장</button>
+    <div v-else class="fetch-error-message">
+      서버와 통신중에 오류가 발생했습니다!
     </div>
   </div>
 </template>
@@ -60,7 +66,8 @@ export default defineComponent({
       documentTitle: this.$route.params.slug as any,
       documentData: {} as DocumentData,
       content: '',
-      message: '문서 편집'
+      message: '문서 편집',
+      isFetchError: true,
     }
   },
   created() {
@@ -75,6 +82,7 @@ export default defineComponent({
           
           if (response.data.success) {
             this.content = this.documentData.content as string;
+            this.isFetchError = false;
           }
         })
         .catch(error => console.error(error));
@@ -162,6 +170,10 @@ export default defineComponent({
       background-color: $light-primary;
       color: white;
     }
+  }
+
+  .fetch-error-message {
+    color: $danger;
   }
 }
 </style>
